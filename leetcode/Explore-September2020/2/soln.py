ffc6ebf31a8a7f7275138243f94990f3f13d2494
Sai Eashwar K S <saieashwar.ks@gmail.com>
2020-09-03 19:55:34 +0530

    added cleaner soln which i saw online

--- leetcode/Explore-September2020/2/soln.py
+++ leetcode/Explore-September2020/2/soln.py
@@ -10,3 +10,19 @@ class Solution:
                 return True
             i += 1
         return False
+
+   
+"""
+Saw a cleaner solution :
+Explanation:
+After looking at the top submission, I found these and thought I explain how they work:
+If there is a repition in s, it means s can be seen as a combination of (at least) two string t.
+s = t + t
+If we double s, we get "t + t + t + t = 4t". cutting away the first and last letter, we end with " u + t + t + u = u + s + u" with u being whatever is left (could be empty).
+But because s is at least 2t, 2s is 4t and taking away the first and last letter only "destroys" 2t, so the string still contains s.
+
+
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+      return s in (s + s)[1: -1]
+"""
