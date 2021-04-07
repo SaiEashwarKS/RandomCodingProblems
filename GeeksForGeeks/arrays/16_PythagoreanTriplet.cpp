bc568307a2a26527ffa94eab506b9d4d0219a1dd
Sai Eashwar K S <saieashwar.ks@gmail.com>
2021-04-08 00:21:32 +0530

    Create 16_PythagoreanTriplet.cpp

---
+++ GeeksForGeeks/arrays/16_PythagoreanTriplet.cpp
@@ -0,0 +1,87 @@
+#if 0
+Given an array arr of N integers, write a function that returns true if there is a triplet (a, b, c) that satisfies a2 + b2 = c2, otherwise false.
+
+Example 1:
+
+Input:
+N = 5
+Arr[] = {3, 2, 4, 6, 5}
+Output: Yes
+Explanation: a=3, b=4, and c=5 forms a
+pythagorean triplet.
+Example 2:
+
+Input:
+N = 3
+Arr[] = {3, 8, 5}
+Output: No
+Explanation: No such triplet possible.
+Your Task:
+Complete the function checkTriplet() which takes an array arr, single integer n, as input parameters and returns boolean denoting answer to the problem. You don't to print answer or take inputs. 
+Note: The driver will print "Yes" or "No" instead of boolean.
+
+Expected Time Complexity: O(max(Arr[i])2)
+Expected Auxiliary Space: O(max(Arr[i]))
+
+Constraints:
+1 <= N <= 10^7
+1 <= Arr[i] <= 1000
+#endif
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	// Function to check if the
+	// Pythagorean triplet exists or not
+	bool checkTriplet(int arr[], int n) {
+	    // code here
+	    sort(arr, arr + n);
+	    for(int i = n-1; i > 1; --i)
+	    {
+	        int l = 0;
+	        int r = i - 1;
+	        int num = arr[i] * arr[i];
+	        while(l < r)
+	        {
+	            int sum = arr[l] * arr[l] + arr[r] * arr[r];
+	            if(sum == num)
+	                return 1;
+	            if(sum > num)
+	                --r;
+	            else
+	                ++l;
+	        }
+	    }
+	    return 0;
+	}
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.checkTriplet(arr, n);
+        if (ans) {
+            cout << "Yes\n";
+        } else {
+            cout << "No\n";
+        }
+    }
+    return 0;
+}
+  // } Driver Code Ends
