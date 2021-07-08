881db12f1cfb7fba1bfe1ce31f1d5be47c054b59
Sai Eashwar K S <saieashwar.ks@gmail.com>
2021-07-09 01:14:09 +0530

    Create 12_SubsetSum.cpp

---
+++ GeeksForGeeks/dynamic_programming/12_SubsetSum.cpp
@@ -0,0 +1,101 @@
+#if 0
+Given an array arr[] of size N, check if it can be partitioned into two parts such that the sum of elements in both parts is the same.
+
+Example 1:
+Input: N = 4
+arr = {1, 5, 11, 5}
+Output: YES
+Explaination: 
+The two parts are {1, 5, 5} and {11}.
+
+Example 2:
+Input: N = 3
+arr = {1, 3, 5}
+Output: NO
+Explaination: This array can never be 
+partitioned into two such parts.
+
+Your Task:
+You do not need to read input or print anything. Your task is to complete the function equalPartition() which takes the value N and the array as input parameters 
+and returns 1 if the partition is possible. Otherwise, returns 0.
+
+
+Expected Time Complexity: O(N*sum of elements)
+Expected Auxiliary Space: O(N*sum of elements)
+
+
+Constraints:
+1 ≤ N ≤ 100
+1 ≤ arr[i] ≤ 1000
+#endif
+
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int equalPartition(int N, int arr[])
+    {
+        // code here
+        int sum = 0;
+        for(int i = 0; i < N; ++i)
+        {
+            sum += arr[i];
+        }
+        if(sum % 2) {
+            return false;
+        }
+        sum /= 2;
+        bool dp[N+1][sum+1];
+        for(int s = 0; s < sum+1; ++s)
+        {
+            dp[0][s] = false;
+        }
+        for(int i = 0; i < N+1; ++i)
+        {
+            dp[i][0] = true;
+        }
+        for(int i = 1; i < N+1; ++i)
+        {
+            for(int s = 1; s < sum+1; ++s)
+            {
+                bool ele_is_sum = arr[i-1] == s;
+                bool prev_subset_true = dp[i-1][s];
+                bool prev_plus_pres_true = false;
+                if(s >= arr[i-1])
+                {
+                    prev_plus_pres_true = dp[i-1][s-arr[i-1]];
+                }
+                dp[i][s] = ele_is_sum || prev_subset_true || prev_plus_pres_true;
+            }
+        }
+        return dp[N][sum];
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int arr[N];
+        for(int i = 0;i < N;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+        if(ob.equalPartition(N, arr))
+            cout<<"YES\n";
+        else
+            cout<<"NO\n";
+    }
+    return 0;
+}  // } Driver Code Ends
